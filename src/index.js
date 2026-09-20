21455d6a2e3078b1f113ff3a07f7a9057f126626
Saroj-Mandal24 <saroj24uec063@gmail.com>
2026-09-20 11:30:55 +0530

    util codes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,15 @@ dotenv.config({
 })
 
 connect_DB()
-
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) => {
+    console.log("Mongo DB connection failed !!!",err);
+})
 
 /*import express from "express";
 const app = express()
